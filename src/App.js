9d2b60e2b6f99a87955ce91606584a4147c08fca
MadhuParappa <madhuhalmath@gmail.com>
2020-12-17 00:06:33 +0530

    message changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Email Team Demo App!!!!!!!!!
+          Email Team Demo App!!!!!!!!!!
         </p>
       </header>
     </div>
